c1f202b116a01b511056980a38f870d7b0e12863
Higo Felipe <higo.pires@ifma.edu.br>
2020-07-21 16:02:31 -0300

    'novo'

--- src/main/java/com/lsdi/social/mhealth/subscriber/AbnormalBehaviorListener.java
+++ src/main/java/com/lsdi/social/mhealth/subscriber/AbnormalBehaviorListener.java
@@ -59,7 +59,7 @@ public class AbnormalBehaviorListener implements UpdateListener {
             e.printStackTrace();
         }
         // Salva o padrão de sociabilidade reconhecido
-        List<List<Integer>> currentPattern = FileUtil.getCurrentPattern("/SocialMHealth/patternsHistory/currentPattern.json").get(context);
+        List<List<Integer>> currentPattern = FileUtil.getCurrentPattern("C:\\Users\\Higo Felipe\\git\\SocialMHealth\\patternsHistory\\currentPattern.json").get(context);
 
         if (currentPattern != null) {
             detectDrift(sociabilityPatterns, currentPattern);
